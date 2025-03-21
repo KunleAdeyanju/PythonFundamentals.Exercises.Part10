e9376defb4e67efdf7aa9e74a400c76ee12386b6
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-21 14:33:24 -0400

    Loads of stuff have been done

---
+++ persistent_bank_tester.py
@@ -0,0 +1,1 @@
+from persistent_small_town_teller import Person, Account, Bank2
---
+++ persistent_small_town_teller.py
@@ -0,0 +1,32 @@
+import small_town_teller
+import json
+import pickle
+import os
+
+class PersistenceUtils():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def write_pickle(file_path, data):
+        with open(file_path, "wb") as handler:
+            pickle.dump(data, handler)
+    
+
+    @staticmethod
+    def load_pickle(file_path):
+        with open(file_path, "rb") as handler:
+            data = pickle.load(handler)
+        return data
+
+
+class Bank2(small_town_teller.Bank, PersistenceUtils):
+    
+    def save_data(self):
+        self.write_pickle("/Users/kunle/Projects/PythonFundamentals.Exercises.Part10/customer.pickle", self.customer)
+        self.write_pickle("/Users/kunle/Projects/PythonFundamentals.Exercises.Part10/accounts.pickle", self.account)
+
+    def load_data(self):
+        self.load_pickle("/Users/kunle/Projects/PythonFundamentals.Exercises.Part10/customer.pickle")
+        self.load_pickle("/Users/kunle/Projects/PythonFundamentals.Exercises.Part10/accounts.pickle")
