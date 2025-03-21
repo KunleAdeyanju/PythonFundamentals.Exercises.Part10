ba3e790861f13dee7419d38cc81663144865f8a4
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-03-20 20:18:53 -0400

    seleton formed

---
+++ small_town_teller.py
@@ -0,0 +1,32 @@
+
+class Person:
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+class Account:
+    def __init__(self, number, type, owner, balance):
+        self.number = number
+        self.type = type
+        self.owner = owner
+        self.balance = balance
+
+class Bank:
+    def new_customer():
+        pass
+
+    def new_account():
+        pass
+
+    def remove_account():
+        pass
+
+    def deposit():
+        pass
+
+    def withdrawal():
+        pass
+
+    def balance_inquiry():
+        pass
